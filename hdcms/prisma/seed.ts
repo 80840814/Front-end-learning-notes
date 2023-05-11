8afcd08c71e590f8594dd946d5e61b48fae5133d
yangmo <80840814@qq.com>
2023-05-11 10:50:26 +0800

    创建数据表并使用mock填充数据

---
+++ hdcms/prisma/seed.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client'
+import { Random } from 'mockjs'
+const prisma = new PrismaClient()
+async function run() {
+  for (let i = 0; i < 30; i++) {
+    await prisma.user.create({
+      data: {
+        name: Random.name(),
+        password: Random.string(),
+        App: {
+          create: {
+            title: Random.csentence(),
+            content: Random.cparagraph(),
+            preview: Random.image('300x300'),
+          },
+        },
+      },
+    })
+  }
+}
+run()
