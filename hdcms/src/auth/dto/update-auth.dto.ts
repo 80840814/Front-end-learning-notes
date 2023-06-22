8ba40a5f4d1a7a6ca0b0318c58834ae277cf2eaf
yangmo <80840814@qq.com>
2023-06-22 14:29:57 +0800

    清空hdcms数据

--- hdcms/src/auth/dto/update-auth.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { RegisterDto } from './resister.dto';
-
-export class UpdateAuthDto extends PartialType(RegisterDto) {}
--- hdcms/src/config.ts
+++
@@ -1,7 +0,0 @@
-import { registerAs } from '@nestjs/config'
-export default registerAs('app', () => {
-  return {
-    name: 'app',
-    address: 'beijing',
-  }
-})
